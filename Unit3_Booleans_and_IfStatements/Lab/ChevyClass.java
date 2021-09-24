3dc369c1920b7db58c96c4e0f1cefe51099293a9
sammyseetharam <90780132+sammyseetharam@users.noreply.github.com>
2021-09-23 20:03:26 -0600

    up to toString method
    u3 lab

--- Unit3_Booleans_and_IfStatements/Lab/ChevyClass.java
+++ Unit3_Booleans_and_IfStatements/Lab/ChevyClass.java
@@ -2,10 +2,6 @@ package Unit3_Booleans_and_IfStatements.Lab;
 
 public class ChevyClass {
     //instance variables
-
-    //year, mileage, fuel efficiency, price, model, color, luxury package status, 4WD package status, and sports package status (constructor is in this same order!)
-    //Vehicle Constants: vehicle make of “Chevrolet”, tax rate of 12.2%, appropriate luxury package price increase, sports price increase, and fuel efficiency deduction (see calcPrice() method description)
-
     private int year;
     private int mileage;
     private double fuelEfficiency;
@@ -15,7 +11,6 @@ public class ChevyClass {
     private boolean luxPackageStatus;
     private boolean fourWDPack;
     private boolean sportsPack;
-
     //constants
     final private String make = "Chevrolet";
     final private double taxRate = .122 * this.price; //percentage as a double
@@ -23,6 +18,69 @@ public class ChevyClass {
     final private int fourWDRate = 3500 + this.price;
     final private double sprtRate = 0.15 * this.price;
     final private double sprtFuel = this.fuelEfficiency - (this.fuelEfficiency * 0.2);
+    //default constructor
+    public ChevyClass(){
+        year = 2021;
+        mileage = 0;
+        color = "White";
+        model = "Trax";
+        fuelEfficiency = 35;
+        price = 16000;
+        luxPackageStatus = false;
+        fourWDPack = false;
+        sportsPack = false;
+    }
+    //Full Constructor
+    public ChevyClass(int year, int mileage, double fuelEfficiency, int price, String model, String color, boolean luxPackageStatus, boolean fourWDPack, boolean sportsPack) {
+        this.year = year;
+        this.mileage = mileage;
+        this.fuelEfficiency = fuelEfficiency;
+        this.price = price;
+        this.model = model;
+        this.color = color;
+        this.luxPackageStatus = luxPackageStatus;
+        this.fourWDPack = fourWDPack;
+        this.sportsPack = sportsPack;
+    }
+    //compareTo method
+    public boolean compareTo(ChevyClass other){
+        return(this.price == other.price);
+    }
+    //equals method
+    public String equals(ChevyClass other){
+        if(this.model.equals(other.model) && this.color.equals(other.color)){
+            if(this.mileage < 200 && other.mileage < 200 ||
+               this.mileage >= 200 && other.mileage >= 200){
+            return "equal";
+            }
+        }
+        return "not equal";
+    }
+    //toString method
+    public String toString(){
+        String toString = year + " " + make + " " + model + " " + "(" + color + ")";
+        toString += "\n\tPRICE\t\t" + "$" + price;
+        toString += "\n\tMILES\t\t" + mileage;
+        toString += "\n\tFUEL EFFICIENCY\t" + fuelEfficiency + " mpg";
+        toString += "\n\tPACKAGES:";
+
+        if(luxPackageStatus == true){
+            toString += "\n\t-Luxury Package";
+        }
+        if(fourWDPack == true){
+            toString += "\n\t-4WD Package";
+        }
+        if(sportsPack == true){
+            toString += "\n\t-Sports Package";
+        }else{
+            toString += "\n\t-None";
+        }
+        return toString;
+    }
+
+
+
 
 
 }
+
