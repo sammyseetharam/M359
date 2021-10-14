1cb712c3167e2210281eca86fae6e70e0001d2a4
sammyseetharam <90780132+sammyseetharam@users.noreply.github.com>
2021-10-14 10:09:55 -0500

    java lab edit #1

---
+++ Unit4_Iteration/lab/PigLatin.java
@@ -0,0 +1,21 @@
+package Unit4_Iteration.lab;
+
+public class PigLatin {
+    /*
+    public Static String toPigLatin(String phrase){}
+
+    public Static String tra
+
+
+    */
+    public static boolean ifVowel(String str){
+        String vowelList = "aeiouAEIOU";
+
+        for(int i = 0; i < str.length() - 1; i ++){
+            if(str.charAt(i) == vowelList)
+        }
+
+
+        }
+    }
+}
