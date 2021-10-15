aa1f68a3a54cf06a1ffce26623db1dde6b727e8c
sammyseetharam <90780132+sammyseetharam@users.noreply.github.com>
2021-10-14 20:50:42 -0600

    new updates to the lab
    New functions written for the pig latin lab

--- Unit4_Iteration/lab/PigLatin.java
+++ Unit4_Iteration/lab/PigLatin.java
@@ -1,21 +1,41 @@
 package Unit4_Iteration.lab;
 
 public class PigLatin {
-    /*
-    public Static String toPigLatin(String phrase){}
 
-    public Static String tra
+    public static String toPigLatin(String phrase){
+        String finalResult = "";
+        for (int i = 0; i < phrase.length() - 1; i++) {
+            if(phrase.charAt(i) == ' '){
 
-
-    */
-    public static boolean ifVowel(String str){
-        String vowelList = "aeiouAEIOU";
-
-        for(int i = 0; i < str.length() - 1; i ++){
-            if(str.charAt(i) == vowelList)
-        }
-
-
+            }
         }
     }
+
+    public static String translateWordToPigLatin(String str){
+        String result = "";
+        if(ifVowel(str) == true){
+            //translates vowels to pig latin
+            result = str + "yay";
+        }else{
+            //translates a consonant to pig latin
+            result = str.substring(1) + str.substring(0, 1) + "ay";
+        }
+        return result;
+    }
+
+    public static boolean ifVowel(String str) {
+        String vowelList = "aeiouAEIOU";
+        boolean checkCondition = false;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == vowelList.charAt(i)) {
+                checkCondition = true;
+            } else {
+                checkCondition = false;
+            }
+        }
+        return checkCondition;
+    }
+
+
 }
+
--- Unit4_Iteration/lab/PigLatinTester.java
+++ Unit4_Iteration/lab/PigLatinTester.java
@@ -20,4 +20,5 @@ public class PigLatinTester {
         System.out.println();
     }
 */
+    }
 }
