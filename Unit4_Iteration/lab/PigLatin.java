ce65506b4d6c8dc114fe9d6acef8c332c38d2e44
sammyseetharam <90780132+sammyseetharam@users.noreply.github.com>
2021-10-18 10:09:37 -0500

    code updates

--- Unit4_Iteration/lab/PigLatin.java
+++ Unit4_Iteration/lab/PigLatin.java
@@ -2,41 +2,51 @@ package Unit4_Iteration.lab;
 
 public class PigLatin {
 
-    public static String toPigLatin(String phrase){
+    public static String toPigLatin(String phrase) {
         String finalResult = "";
-        int count = 0;
-        for(int i = 0; i < phrase.length() - 1; i++) {
-           if(phrase.charAt(i + 1) == ' '){
-               finalResult += translateWordToPigLatin(phrase.substring(count, i + 1));
-               count += i + 1;
-           }
+        phrase = phrase + " ";
+        boolean isDone = false;
+
+        while (!isDone) {
+            if (phrase.indexOf(' ') > 0) {
+                finalResult += translateWordToPigLatin(phrase.substring(0, phrase.indexOf(' ')));
+                finalResult += " ";
+                phrase = phrase.substring(phrase.indexOf(' ') + 1);
+            } else {
+                isDone = true;
+            }
         }
         return finalResult;
     }
 
-    public static String translateWordToPigLatin(String str){
+    public static String translateWordToPigLatin(String str) {
         String result = "";
-        if(ifVowel(str) == true){
+        if (ifVowel(str) == true) {
             //translates vowels to pig latin
             result = str + "yay";
-        }else{
+        } else {
             //translates a consonant to pig latin
-            result = str.substring(1) + str.substring(0, 1) + "ay";
-        }
-        return result;
-    }
-
-    public static boolean ifVowel(String str) {
-        String vowelList = "aeiouAEIOU";
-        boolean checkCondition = false;
-        for (int i = 0; i < str.length() - 1; i++) {
-            if (str.charAt(i) == vowelList.charAt(i)) {
-                checkCondition = true;
-            } else {
-                checkCondition = false;
+            for (int i = 0; i < str.length() - 1; i++) {
+                if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+                    result += str.substring(i) + str.substring(0, i) + "ay";
+                }
             }
+            return result;
         }
-        return checkCondition;
+
+        public static boolean ifVowel (String str){
+            String vowelList = "aeiouAEIOU";
+            boolean checkCondition = false;
+            for (int i = 0; i < vowelList.length(); i++) {
+                if (str.charAt(0) == vowelList.charAt(i)) {
+                    checkCondition = true;
+                }else{
+                    checkCondition = false; 
+                }
+            }
+            return checkCondition;
+        }
     }
 }
+
 
