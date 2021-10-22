375b11b91801d916e460d2fdf610f48896336f34
sammyseetharam <90780132+sammyseetharam@users.noreply.github.com>
2021-10-21 22:27:21 -0600

    updates to 4.3 challenge problems
    re- written and challenge 3

--- Unit4_Iteration/examples/Four_Point_Five_Notes.java
+++ Unit4_Iteration/examples/Four_Point_Five_Notes.java
@@ -3,7 +3,7 @@ package Unit4_Iteration.examples;
 import java.sql.SQLOutput;
 
 public class Four_Point_Five_Notes {
-    public static void outputBreak(String name){
+    public static void outputBreak(String name) {
         System.out.println();
         System.out.println();
         System.out.println();
@@ -11,52 +11,43 @@ public class Four_Point_Five_Notes {
         System.out.println();
     }
 
-    public static void everOtherLetterPyramid(String str) {
-        boolean isDone = true;
+    //Challenge #3
 
-            while(isDone){
-                System.out.println(str);
-                for (int i = str.length() - 1; i > 0 ; i-=2) {
-                    for (int j = 0; j <= i; j+= 2) {
-                        System.out.print(str.charAt(j));
-                    }
-                    System.out.println();
-                }
-                isDone = false;
+    public static void everyOtherLetterPyramid(String str){
+
+        while(str.length() > 1) {
+            System.out.println(str);
+            String temp = "";
+            for (int i = 0; i < str.length(); i += 2) {
+                temp += str.charAt(i);
             }
-
-            //Class code
-
-        String orgStr = str;
-        System.out.println();
+            str = temp;
         }
+        System.out.println(str);
+    }
+
     public static void main(String[] args) {
-        everOtherLetterPyramid("FremdVikings");
-        System.out.println();
-        //Challenge #1
-        outputBreak("Challenge 1: Two for loops to go from 1 - 5 in decrementing order");
-        //Runs a total of 5 - 1 times in parent loop
-        //The value j Runs from 1 - value of i, and prints out j each time
-        for (int i = 5; i > 0  ; i--) {
-            for (int j = 1; j <= i ;j++) {
+
+        //Challenge #1 use 2 loops to generate the following output
+        outputBreak("This is challenge 1");
+        for (int i = 5; i > 0; i--) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(j);
             }
             System.out.println();
         }
-        System.out.println();
 
-        //Challenge #2
-        outputBreak("Challenge 2: Two for loops to print a number its correlating amount of times in decrementing order");
-        for (int i = 5; i > 0; i--) {
-            for (int j = 1; j <= i; j++) {
+        outputBreak("This is challenge #2");
+        for(int i = 5; i > 0; i--){
+            for(int j = 1; j <= i; j++){
                 System.out.print(i);
             }
             System.out.println();
         }
-
-        //Challenge #3
-
+        outputBreak("This is challenge #3");
+        everyOtherLetterPyramid("FremdVikings");
 
 
     }
 }
+
