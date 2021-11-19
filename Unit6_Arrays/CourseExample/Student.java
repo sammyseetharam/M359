c4682c791f06e87c8d705d76cd9da71c2c24986a
sammyseetharam <90780132+sammyseetharam@users.noreply.github.com>
2021-11-18 20:48:45 -0700

    commit
    push

--- Unit6_Arrays/CourseExample/Student.java
+++ Unit6_Arrays/CourseExample/Student.java
@@ -1,6 +1,17 @@
 package Unit6_Arrays.CourseExample;
 
 public class Student {
-    
+   private String name;
+   private Course[] myClasses;
+
+   public Student(String name) {
+        this.name = name;
+        myClasses = new Course[8]; //8 periods a day}
+   }
+
+   public Student(String name, Course[] myClasses) {
+        this.name = name;
+        this.myClasses = myClasses;
+   }
 
 }
