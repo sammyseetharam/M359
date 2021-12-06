169f9c9e04dfb34ab9aab1ffe609751cbd97c0ad
sammyseetharam <90780132+sammyseetharam@users.noreply.github.com>
2021-12-06 15:17:43 -0600

    commit

--- Unit6_Arrays/TriviaGame/TriviaDriver.java
+++ Unit6_Arrays/TriviaGame/TriviaDriver.java
@@ -1,16 +1,28 @@
 package Unit6_Arrays.TriviaGame;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class TriviaDriver {
     public static void main(String[] args) throws FileNotFoundException{
         TriviaGame myNewGame = new TriviaGame();
         Question[] questions = myNewGame.getAllQuestions();
 
-        for (Question s: questions) {
+        System.out.println("welcome to the party it gon be lit!");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Do you want a question?");
+        int lol = 0;
+        System.out.println(questions[lol]);
+        /*for (Question s: questions) {
             System.out.println(s);
-        }
+        }*/
+
+
     }
+
+
+
 
 
 }
