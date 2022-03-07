2c5772bf70b8b1c5240cefcbdb029b7af6ced3f8
sammyseetharam <90780132+sammyseetharam@users.noreply.github.com>
2022-03-07 09:26:52 -0600

    commit
    push

---
+++ Unit9_Inheritance/examples/AnimalTester.java
@@ -0,0 +1,35 @@
+package unit_9.in_class.animals;
+
+/**
+* Lesson 7-5: Polymorphism
+ * The purpose of this tester class is to see what happens when an array
+ * of Animal references is assigned objects of type Animal, Pig, and Cow
+ * 
+ * @author   Mrs. Denna 
+ */
+public class AnimalTester
+{
+   public static void main(String[] args)
+   {
+       Animal[] barn = new Animal[6];
+       // 1. What data type does this array hold?
+
+
+       barn[0] = new Animal("Murphy");
+       barn[1] = new Cow("Daisy", "Dairy");
+       barn[2] = new Pig("Wilbur", false);
+       barn[3] = new Cow("Billy", "Beef");
+       barn[4] = new Animal("Maggie");
+       barn[5] = new Pig("Max", true);
+
+       // 2. Did I ONLY store Animal objects in this array?  If not, why do you
+       //    think this was allowed?
+   
+       for (Animal a: barn)
+            a.speak();
+            // 3. What method is getting called here?  The speak method of the Animal class?
+
+       // 4. Try to summarize in 2-3 sentences WHAT you observed in this code.
+   }
+   
+}
---
+++ Unit9_Inheritance/examples/Cow.java
@@ -0,0 +1,22 @@
+package unit_9.in_class.animals;
+
+public class Cow extends Animal
+{
+    private String cowType;                     // Dairy or Beef
+    
+    public Cow(String name, String cowType)
+    {
+        super(name);
+        this.cowType = cowType;
+    }
+
+    /**
+     * Makes a sound for a cow
+     * 
+    */
+    public void speak()
+    {
+        System.out.println("The "+ cowType + " Cow " + getName() + " says MOOOOO.");
+    }
+
+}
