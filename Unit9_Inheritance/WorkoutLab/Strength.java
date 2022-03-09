d08040cf3e5b9a06c73c83a24dd2252a76d0f77e
sammyseetharam <90780132+sammyseetharam@users.noreply.github.com>
2022-03-08 19:56:38 -0700

    pushing p
    gunna

--- Unit9_Inheritance/WorkoutLab/Strength.java
+++ Unit9_Inheritance/WorkoutLab/Strength.java
@@ -3,5 +3,21 @@ package Unit9_Inheritance.WorkoutLab;
 public class Strength extends Workout{
     private int weight;
 
+    public Strength(String name, int workoutNum, int duration, int weight) {
+        super(name, workoutNum, duration);
+        this.weight = weight;
+    }
 
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int StartExercise(){
+        int random = (int)(Math.random() * 3) + 1;
+        return random * weight;
+    }
 }
--- Unit9_Inheritance/WorkoutLab/Wellness.java
+++ Unit9_Inheritance/WorkoutLab/Wellness.java
@@ -1,4 +1,23 @@
 package Unit9_Inheritance.WorkoutLab;
 
-public class Wellness {
+public class Wellness extends Workout{
+    private int numStretches;
+
+    public Wellness(String name, int workoutNum, int duration, int numStretches) {
+        super(name, workoutNum, duration);
+        this.numStretches = numStretches;
+    }
+
+    public int StartExercise(){
+        int random = (int)(Math.random() * 6) + 5;
+        return random * numStretches;
+    }
+
+    public int getNumStretches() {
+        return numStretches;
+    }
+
+    public void setNumStretches(int numStretches) {
+        this.numStretches = numStretches;
+    }
 }
